a45695811307e60b0d4aec2f825068416f6f2ddf
Diego Lopes <oliveira.diegolopes@gmail.com>
2022-05-17 01:29:43 -0300

    Creating arflow structure

--- dags/etl_from_file_to_mongodb.py
+++ dags/etl_from_file_to_mongodb.py
@@ -1,15 +1,55 @@
-# airflow related
-
+# airflow packeges
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 # others packeges
 from datetime import timedelta, datetime
+import pandas as pd
+import xlwings as xw
 
 
-def teste():
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+
+class ExtractDataFromXls:
+  def __init__(self) -> None:
+      pass
+
+  def check_transient_zone():
     pass
 
+  def check_raw_zone():
+    pass
+
+  def check_trusted_zone():
+    pass
+
+  def etl_from_transient_to_raw_zone():
+    pass
+
+  def etl_from_raw_to_trusted_zone():
+    pass
+
+
+  # def teste(file_path, file_name):
+
+  #     with xw.App(visible=False) as app:
+  #     book = xw.Book(file_path.format(file_name))
+  #     book.sheets[0]["C49"].value = "Amazonas"
+
+  #     book.save(file_path.format(file_name))
+  #     book.close()
+      
+
+  #     df = pd.read_excel(
+  #         file_path.format(file_name),
+  #         sheet_name='Plan1',
+  #         usecols='B:W',
+  #         skiprows=52,
+  #         nrows=12
+  #     )
 
 with DAG(
     default_args = {
@@ -20,19 +60,44 @@ with DAG(
         "retry_delay": timedelta(minutes=3),
         "provide_context": True
     },
-    dag_id="teste_dag",
-    description="Responsible for obtaining new customers from sorocaba database and enriching them with API data",
+    dag_id="Extract data from XLS",
+    description="Responsible to extract data from XLS files, transform them and save as parquet file",
     schedule_interval="30 4 * * *",
     catchup=False,
     max_active_runs=1
 ) as dag:
 
+    ObjEtl = ExtractDataFromXls()
 
-    create_temp_path = PythonOperator(
+    check_transient_zone = PythonOperator(
         task_id="teste",
-        python_callable=teste,
+        python_callable=ObjEtl.check_transient_zone,
+        dag=dag
+    )
+
+    check_raw_zone = PythonOperator(
+        task_id="teste",
+        python_callable=ObjEtl.check_raw_zone,
+        dag=dag
+    )
+
+    check_trusted_zone = PythonOperator(
+        task_id="teste",
+        python_callable=ObjEtl.check_trusted_zone,
+        dag=dag
+    )
+
+    etl_from_transient_to_raw_zone = PythonOperator(
+        task_id="teste",
+        python_callable=ObjEtl.etl_from_transient_to_raw_zone,
+        dag=dag
+    )
+
+    etl_from_raw_to_trusted_zone = PythonOperator(
+        task_id="teste",
+        python_callable=ObjEtl.etl_from_raw_to_trusted_zone,
         dag=dag
     )
 
 
-    create_temp_path
+    [check_transient_zone, check_raw_zone, check_trusted_zone] > etl_from_transient_to_raw_zone > etl_from_raw_to_trusted_zone
