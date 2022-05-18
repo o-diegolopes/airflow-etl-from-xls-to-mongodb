bb0495180f5071d9e609a5b2f0b4169debd536f1
Diego Lopes <oliveira.diegolopes@gmail.com>
2022-05-18 01:18:13 -0300

    project structure done

--- dags/etl_from_file_to_mongodb.py
+++ dags/etl_from_file_to_mongodb.py
@@ -7,8 +7,8 @@ from datetime import timedelta, datetime
 from numpy import double
 import pandas as pd
 import xlwings as xw
+from pymongo import MongoClient
 import os, locale, shutil
-
 
 locale.setlocale(locale.LC_ALL, 'pt_BR')
 
@@ -21,13 +21,25 @@ class ExtractDataFromXls:
     self.trusted_zone_path = f"{self.__project_path}/data-lake/trusted-zone"
     self.storage = f"{self.__project_path}/storage"
     self.xls_file = "vendas-combustiveis-m3.xls"
+    self.mongodb_connection = {
+      "host": "mongodb://localhost:27017/",
+      "database": "datalake",
+      "collection": "trusted_zone"
+    }
+    self.final_structure = [
+      'year_month',
+      'product',
+      'uf',
+      'unit',
+      'volume'
+    ]
     self.federation_units = {
       "AC": "Acre",
       "AL": "Alagoas",
       "AP": "Amapá",
       "AM": "Amazonas",
       "BA": "Bahia",
-      "CE": "Ceara",
+      "CE": "Ceará",
       "DF": "Distrito Federal",
       "ES": "Espírito Saanto",
       "GO": "Goiás",
@@ -103,20 +115,25 @@ class ExtractDataFromXls:
     """"
     This function extract oil derivative fuels data from XLS in transient zone to raw zone.
     """
+
+    os.chdir(self.transient_zone_path)
+    os.system(f'start excel.exe {self.xls_file}')
+
     frames = []
     for key, units in self.federation_units.items():
       for product in self.oil_derivative_fuels:
 
         with xw.App(visible=False) as app:
-          book = xw.Book(f"{self.transient_zone_path}/{self.xls_file}")
-          book.sheets[0]["C49"].value = units
-          book.sheets[0]["C50"].value = product
+          file = f"{self.transient_zone_path}/{self.xls_file}"
+          book = xw.Book(file)
+          book.sheets[0]["C49"].value = units.upper()
+          book.sheets[0]["C50"].value = product.upper()
 
           book.save(f"{self.transient_zone_path}/{self.xls_file}")
           book.close()
 
           df = pd.read_excel(
-              f"{self.storage}/{self.xls_file}",
+              f"{self.transient_zone_path}/{self.xls_file}",
               sheet_name=self.oil_derivative_fuels_table.get("sheet"),
               usecols=self.oil_derivative_fuels_table.get("usecols"),
               skiprows=self.oil_derivative_fuels_table.get("skiprows"),
@@ -134,19 +151,23 @@ class ExtractDataFromXls:
             lambda row: f"{str(row['level_1'])}-{str(get_month(row['Dados']) if get_month(row['Dados']) >= 10 else '0'+ str(get_month(row['Dados'])))}", axis=1
           )
           df["volume"] = df.apply(lambda row: round(float(row['volume']), 3), axis=1)
-          df["create_at"] = datetime.today.strftime('%Y-%m-%d')
+          df["create_at"] = datetime.today().strftime('%Y-%m-%d')
           
           df = df.drop(["Dados", "level_1"],  axis=1).set_index('year_month').reset_index()
 
           frames.append(df)
           
     dataframe = pd.concat(frames)
-    dataframe.to_csv(f"{self.raw_zone_path}/oil_derivative_fuels.csv")
+    dataframe.to_csv(f"{self.raw_zone_path}/oil_derivative_fuels.csv", index=False)
 
   def extract_diesel_fuel_data_to_raw_zone(self, *args, **kwargs):
     """"
     This function extract diesel fuel data from XLS in transient zone to raw zone.
     """
+
+    os.chdir(self.transient_zone_path)
+    os.system(f'start excel.exe {self.xls_file}')
+    
     frames = []
     for key, units in self.federation_units.items():
       for product in self.diesel_types:
@@ -160,7 +181,7 @@ class ExtractDataFromXls:
           book.close()
 
           df = pd.read_excel(
-              f"{self.storage}/{self.xls_file}",
+              f"{self.transient_zone_path}/{self.xls_file}",
               sheet_name=self.diesel_table.get("sheet"),
               usecols=self.diesel_table.get("usecols"),
               skiprows=self.diesel_table.get("skiprows"),
@@ -185,92 +206,113 @@ class ExtractDataFromXls:
           frames.append(df)
           
     dataframe = pd.concat(frames)
-    dataframe.to_csv(f"{self.raw_zone_path}/diesel_fuels.csv")
+    dataframe.to_csv(f"{self.raw_zone_path}/diesel_fuels.csv", index=False)
   
+  def copy_data_from_raw_to_trusted_zone(self, *args, **kwargs):
+    """"
+    This function make a copy of files in storage to transiente zone.
+    """
+    files = os.listdir(self.raw_zone_path)
+    for file in files:
+      shutil.copyfile(f"{self.raw_zone_path}/{file}", f"{self.trusted_zone_path}/{file}")
   
-  
-  def load_data_to_mongodb(*args, **kwargs):
-    pass
+  def load_data_to_mongodb(self, db_url='localhost', db_port=27017, *args, **kwargs):
+    """"
+    This function load data to trusted zone.
+    """
+
+    client = MongoClient(self.mongodb_connection.get("host"))
+    db = client[self.mongodb_connection.get("database")]
+    coll = db[self.mongodb_connection.get("collection")]
+
+    columns = self.final_structure
+
+    files = os.listdir(self.trusted_zone_path)
+    for file in files:
+      data = pd.read_csv(f"{self.trusted_zone_path}/{file}", names=columns)
+      data = data.to_dict()
+      x = coll.insert_one(data)
 
   def clear_transient_zone(self, *args, **kwargs):
     shutil.rmtree(f"{self.transient_zone_path}")
 
 etl = ExtractDataFromXls()
 
-etl.clear_transient_zone()
-etl.check_if_exists_transient_zone()
-etl.copy_data_from_storage_to_transient_zone()
-etl.extract_diesel_fuel_data_to_raw_zone()
+dag = DAG(
+    default_args = {
+        "owner": "Diego Lopes",
+        "depends_on_past": False,
+        "start_date": datetime(2022, 5, 17),
+        "retries": 3,
+        "retry_delay": timedelta(minutes=3),
+        "provide_context": True
+    },
+    dag_id="extract_data_from_xls",
+    description="Responsible to extract data from XLS files, transform them and save as parquet file",
+    schedule_interval="05 15 * * *",
+    catchup=False,
+    max_active_runs=1
+)
 
-# dag = DAG(
-#     default_args = {
-#         "owner": "Diego Lopes",
-#         "depends_on_past": False,
-#         "start_date": datetime(2022, 5, 17),
-#         "retries": 3,
-#         "retry_delay": timedelta(minutes=3),
-#         "provide_context": True
-#     },
-#     dag_id="extract_data_from_xls",
-#     description="Responsible to extract data from XLS files, transform them and save as parquet file",
-#     schedule_interval="05 15 * * *",
-#     catchup=False,
-#     max_active_runs=1
-# )
+ObjEtl = ExtractDataFromXls()
 
-# ObjEtl = ExtractDataFromXls()
+check_if_exists_transient_zone = PythonOperator(
+    task_id="check_if_exists_transient_zone",
+    python_callable=ObjEtl.check_if_exists_transient_zone,
+    dag=dag
+)
 
-# check_if_exists_transient_zone = PythonOperator(
-#     task_id="check_if_exists_transient_zone",
-#     python_callable=ObjEtl.check_if_exists_transient_zone,
-#     dag=dag
-# )
+check_if_exists_raw_zone = PythonOperator(
+    task_id="check_if_exists_raw_zone",
+    python_callable=ObjEtl.check_if_exists_raw_zone,
+    dag=dag
+)
 
-# check_if_exists_raw_zone = PythonOperator(
-#     task_id="check_if_exists_raw_zone",
-#     python_callable=ObjEtl.check_if_exists_raw_zone,
-#     dag=dag
-# )
+check_if_exists_trusted_zone = PythonOperator(
+    task_id="check_if_exists_trusted_zone",
+    python_callable=ObjEtl.check_if_exists_trusted_zone,
+    dag=dag
+)
 
-# check_if_exists_trusted_zone = PythonOperator(
-#     task_id="check_if_exists_trusted_zone",
-#     python_callable=ObjEtl.check_if_exists_trusted_zone,
-#     dag=dag
-# )
+copy_data_from_storage_to_transient_zone = PythonOperator(
+    task_id="copy_data_from_storage_to_transient_zone",
+    python_callable=ObjEtl.copy_data_from_storage_to_transient_zone,
+    dag=dag
+)
 
+extract_oil_derivative_fuels_data_to_raw_zone = PythonOperator(
+    task_id="extract_oil_derivative_fuels_data_to_raw_zone",
+    python_callable=ObjEtl.extract_oil_derivative_fuels_data_to_raw_zone,
+    dag=dag
+)
 
-# extract_data_from_xls_to_transient = PythonOperator(
-#     task_id="extract_data_from_xls_to_transient",
-#     python_callable=ObjEtl.extract_data_from_xls_to_transient,
-#     dag=dag
-# )
+extract_diesel_fuel_data_to_raw_zone = PythonOperator(
+    task_id="extract_diesel_fuel_data_to_raw_zone",
+    python_callable=ObjEtl.extract_diesel_fuel_data_to_raw_zone,
+    dag=dag
+)
 
-# consolidate_data_and_load_to_raw = PythonOperator(
-#     task_id="consolidate_data_and_load_to_raw",
-#     python_callable=ObjEtl.consolidate_data_and_load_to_raw,
-#     dag=dag
-# )
+copy_data_from_raw_to_trusted_zone = PythonOperator(
+    task_id="copy_data_from_raw_to_trusted_zone",
+    python_callable=ObjEtl.copy_data_from_raw_to_trusted_zone,
+    dag=dag
+)
 
-# transform_data_from_raw_to_trusted = PythonOperator(
-#     task_id="transform_data_from_raw_to_trusted",
-#     python_callable=ObjEtl.transform_data_from_raw_to_trusted,
-#     dag=dag
-# )
+load_data_to_mongodb = PythonOperator(
+    task_id="load_data_to_mongodb",
+    python_callable=ObjEtl.load_data_to_mongodb,
+    dag=dag
+)
 
-# load_data_to_mongodb = PythonOperator(
-#     task_id="load_data_to_mongodb",
-#     python_callable=ObjEtl.load_data_to_mongodb,
-#     dag=dag
-# )
+clear_transient_zone = PythonOperator(
+    task_id="clear_transient_zone",
+    python_callable=ObjEtl.clear_transient_zone,
+    dag=dag
+)
 
-# clear_transient_zone = PythonOperator(
-#     task_id="clear_transient_zone",
-#     python_callable=ObjEtl.clear_transient_zone,
-#     dag=dag
-# )
-
-# extract_data_from_xls_to_transient.set_upstream([check_if_exists_transient_zone, check_if_exists_raw_zone, check_if_exists_trusted_zone])
-# extract_data_from_xls_to_transient.set_downstream(consolidate_data_and_load_to_raw)
-# consolidate_data_and_load_to_raw.set_downstream(transform_data_from_raw_to_trusted)
-# transform_data_from_raw_to_trusted.set_downstream(load_data_to_mongodb)
-# load_data_to_mongodb.set_downstream(clear_transient_zone)
+copy_data_from_storage_to_transient_zone.set_upstream([check_if_exists_transient_zone, check_if_exists_raw_zone, check_if_exists_trusted_zone])
+copy_data_from_storage_to_transient_zone.set_downstream(extract_oil_derivative_fuels_data_to_raw_zone)
+extract_oil_derivative_fuels_data_to_raw_zone.set_downstream(extract_diesel_fuel_data_to_raw_zone)
+copy_data_from_raw_to_trusted_zone.set_upstream(extract_diesel_fuel_data_to_raw_zone)
+copy_data_from_raw_to_trusted_zone.set_downstream(load_data_to_mongodb)
+load_data_to_mongodb.set_downstream(clear_transient_zone)
